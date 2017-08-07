b7449ff7d643da731e69b3690540894a2be4a0d9
Ross Melville <rossmelville@hotmail.com>
2017-08-07 14:48:30 +0100

    Initial commit. Movie class added with constructor method.

---
+++ app/src/main/java/com/example/movieranking/Movie.java
@@ -0,0 +1,20 @@
+package com.example.movieranking;
+
+/**
+ * Created by rossmelville on 07/08/2017.
+ */
+
+public class Movie {
+
+    String title;
+    String genre;
+    int ranking;
+
+    public Movie(String title, String genre, int ranking) {
+        this.title = title;
+        this.genre = genre;
+        this.ranking = ranking;
+    }
+
+
+}
